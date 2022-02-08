95a95a942564d8a27df13854d5a26311a46ac8e8
npick001 <npick001@odu.edu>
2022-02-08 15:50:47 -0500

    Base Code

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+double LCG(double Xi, double multiplier, double increment, double modulus){
+
+}
+
+int main(){
+    // Variables needed
+    // Xi and nextX
+    // multiplier, increment, modulus
+    // (aXi + c) * modulus
+    double Xi, nextX, multiplier, increment, modulus;
+    std::cout << "This is a RNG based off a LCG." << '\n';
+    std::cout << "The formula for generating the random numbers is "
+    << "((multiplier) * Xi + increment) * modulus";
+    std::cout << "Enter a starting point, multiplier, increment, and modulus in that order." << '\n';
+    std::cin >> Xi >> multiplier >> increment >> modulus;
+    nextX = LCG(Xi, multiplier, increment, modulus);
+    std::cout << "Random number: " << nextX;
+}
